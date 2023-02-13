0b4942263d45055015971b3eda00202756089d20
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-02-13 17:13:05 -0300

    crear componentes

--- notesR/src/App.jsx
+++ notesR/src/App.jsx
@@ -1,39 +1,19 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import login from './components/login';
+import signup from './components/signup';
+import notes from './components/notes';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is
-          {' '}
-          {count}
-        </button>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.jsx</code>
-          {' '}
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Routes>
+      <Route path="/" element={<login />} exact />
+      <Route path="/signup" element={<signup />} exact />
+      <Route path="/notes" element={<notes />} exact />
+    </Routes>
   );
 }
 
