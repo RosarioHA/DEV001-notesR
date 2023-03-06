29c18355d2f3e3a5e126d9fc1a50adc43851f326
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-03-06 20:25:04 -0300

    actualiza lista al agregar elementos

--- src/components/notesList.jsx
+++ src/components/notesList.jsx
@@ -2,11 +2,21 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import React, {
-  useEffect, useState, useContext, createContext,
+  useEffect, useState,
 } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import {
-  collection, addDoc, getDocs, orderBy, doc, deleteDoc, getDoc, setDoc,
+  collection,
+  addDoc,
+  getDocs,
+  orderBy,
+  query,
+  onSnapshot,
+  where,
+  doc,
+  deleteDoc,
+  getDoc,
+  setDoc,
 } from 'firebase/firestore';
 import { db, auth } from '../firebase/firebase-init';
 import '../styles/List.css';
@@ -14,42 +24,49 @@ import '../styles/List.css';
 function NotesList() {
   // state variables
   const [list, setList] = useState([]);
-  // let usid;
+  const [usid, setUsid] = useState('');
 
-  // el problema con el renderizado es que no detecta al usuario antes de tomar
-  // getList en el useEffect. puede hacerse con un setuser denuevo. Ivy
-  // recomienda hacer un contexto con el usuario para llamarlo de manera gloval.
-  // tambien  recomienda filtrar los comentarios en lugar de hacer una coleccion para c/u
+  onAuthStateChanged(auth, (user) => {
+    const { uid } = user;
+    if (uid !== null) {
+      // setUsid(uid);
+      setUsid(uid);
+    }
+  });
 
-  // onAuthStateChanged(auth, (user) => {
-  //   const { uid } = user;
-  //   if (uid !== null) {
-  //     // setUsid(uid);
-  //     usid = uid;
+  // const getList = async () => {
+  //   try {
+  //     // const collectionName = 'nCollection';
+  //     const nCollectionQuery = query(collection(db, 'nCollection'), orderBy('date'));
+  //     const querySnapshot = await getDocs(nCollectionQuery);
+  //     const docs = [];
+  //     querySnapshot.forEach((docu) => {
+  //       docs.push({ ...docu.data(), id: docu.id });
+  //     });
+  //     setList(docs);
+  //   } catch (error) {
+  //     console.log(error);
   //   }
-  // });
+  // };
 
-  const getList = async () => {
-    try {
-      const collectionName = 'nCollection';
-      const querySnapshot = await getDocs(
-        collection(db, collectionName),
-        orderBy('date', 'desc'),
-      );
-      console.log(collectionName);
+  const listenForUpdates = () => {
+    const collectionName = 'nCollection';
+    const nCollectionQuery = query(
+      collection(db, collectionName),
+      orderBy('date'),
+    );
+    onSnapshot(nCollectionQuery, (snapshot) => {
       const docs = [];
-      querySnapshot.forEach((docu) => {
-        docs.push({ ...docu.data(), id: docu.id });
+      snapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
       });
       setList(docs);
-    } catch (error) {
-      console.log(error);
-    }
+    });
   };
 
   // variables that render the notes list
   useEffect(() => {
-    getList();
+    listenForUpdates();
   }, []);
 
   return (
